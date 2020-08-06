ebb405646b7a6b8817db2286837d2c854094cf96
Ryan Quey <quey.ryan@gmail.com>
2020-08-06 01:59:08 -0700

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@
 #target/universal/scripts/bin/cambodia-in-charts \
 cambodia-in-charts-1.0-SNAPSHOT/bin/cambodia-in-charts \
   -Dhttp.port=21379 \
-  -Dplay.http.secret.key=ad31779d4ee49d5ad5162bf1429c32e2e9933f3b \
+  -Dplay.http.secret.key=ad31779d4ee49d5ad5162bf1429c32e2e9933f3b
